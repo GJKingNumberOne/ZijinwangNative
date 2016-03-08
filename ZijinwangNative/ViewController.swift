27e5cfa996d6d6da7e293151159cf176fdc9af4a
华瑞金科 <huaruijinke@huaruijinkedemacbook-pro.local>
2016-03-08 14:30:53 +0800

    Initial Commit

---
+++ ZijinwangNative/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZijinwangNative
+//
+//  Created by 华瑞金科 on 16/3/8.
+//  Copyright © 2016年 华瑞金科. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
